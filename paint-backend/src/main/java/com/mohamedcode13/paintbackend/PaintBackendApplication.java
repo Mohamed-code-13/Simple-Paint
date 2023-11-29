f98e7d69874bca887f550ddaa69ceaf3ba0655ca
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-11-29 03:03:20 +0200

    Creating the spring boot app.

---
+++ paint-backend/src/main/java/com/mohamedcode13/paintbackend/PaintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mohamedcode13.paintbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintBackendApplication.class, args);
+	}
+
+}
