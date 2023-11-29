525c5d998e240dabfb0e856904bad959f05ac55f
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-11-29 07:55:57 +0200

    Creating a new Vue JS project

---
+++ paint-frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ShapePicker />
+</template>
+
+<script>
+import ShapePicker from "./components/ShapePicker.vue";
+
+export default {
+  name: "App",
+  components: {
+    ShapePicker,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+html,
+body {
+  background-color: #eee;
+  height: 100%;
+}
+</style>
---
+++ paint-frontend/src/components/ShapePicker.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="shapePicker"></div>
+</template>
+
+<script>
+export default {
+  name: "ShapePicker",
+};
+</script>
+
+<style scoped>
+#shapePicker {
+  background: purple;
+  width: 80px;
+  height: 100%;
+}
+</style>
