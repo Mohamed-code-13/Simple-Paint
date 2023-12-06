18c95ecd0bf4b67fbcc7c8f4c56d7f9f5aef9ce2
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-12-06 05:03:37 +0200

    Finishing Dialog with its functionality.

--- paint-frontend/src/App.vue
+++ paint-frontend/src/App.vue
@@ -1,120 +1,236 @@
-<script setup>
-  import{ref} from 'vue'
-  import CanvasComp from './components/CanvasComp.vue'
-  import DialogComp from './components/DialogComp.vue'
-  let selected = ref("none")
-  const save = ()=>{
+<script>
+import { ref } from 'vue'
+import CanvasComp from './components/CanvasComp.vue'
+import DialogComp from './components/DialogComp.vue'
+const save = () => {}
+const load = () => {}
+const applyChangesRef = ref(0)
 
+export default {
+  data() {
+    return {
+      showDialog: false,
+      selected: ref('none'),
+      requirements: [],
+      defaultValues: []
+    }
+  },
+  components: {
+    CanvasComp,
+    DialogComp
+  },
+  methods: {
+    changeParameters(values) {
+      this.showDialog = false
+      console.log(values)
+      this.$refs.applyChangesRef.applyChanges('23434')
+    },
+    openDialog(args) {
+      this.requirements = args[0]
+      this.defaultValues = args[1]
+      this.showDialog = true
+    }
   }
-  const load = ()=>{
-    
-  }
+}
 </script>
 
 <template>
   <div id="container">
-    <DialogComp />
+    <DialogComp
+      v-if="showDialog"
+      :requirements="requirements"
+      :default-values="defaultValues"
+      @changeParameters="changeParameters"
+    />
     <div id="toolbox">
       <div id="tool-container">
         <li>
-        <div class="tool" @click="selected='line'"><img width="25" height="25" src="https://img.icons8.com/ios/50/line--v1.png" alt="line--v1"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='square'"><img width="25" height="25" src="https://img.icons8.com/ios/50/square-90.png" alt="square-90"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='circle'"><img width="25" height="25" src="https://img.icons8.com/ios/50/circled.png" alt="circled"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='rectangle'"><img width="25" height="25" src="https://img.icons8.com/material-sharp/24/rectangle-stroked.png" alt="rectangle-stroked"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='triangle'"><img width="25" height="25" src="https://img.icons8.com/material-outlined/24/triangle-stroked.png" alt="triangle-stroked"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='ellipse'"><img width="25" height="25" src="https://img.icons8.com/ios/50/ellipse-stroked--v1.png" alt="ellipse-stroked--v1"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='copy'"><img width="25" height="25" src="https://img.icons8.com/material-outlined/24/copy.png" alt="copy"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='paste'"><img width="25" height="25" src="https://img.icons8.com/material-outlined/24/paste.png" alt="paste"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='delete'"><img width="25" height="25" src="https://img.icons8.com/ios/50/trash--v1.png" alt="trash--v1"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='move'"><img width="25" height="25" src="https://img.icons8.com/ios/50/move.png" alt="move"/></div>
-      </li>
-      <li>
-        <div class="tool" id="undo"><img width="25" height="25" src="https://img.icons8.com/material-outlined/24/undo.png" alt="undo"/></div>
-      </li>
-      <li>
-        <div class="tool" id="redo"><img width="24" height="24" src="https://img.icons8.com/material-outlined/24/redo.png" alt="redo"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="selected='options'"><img width="25" height="25" src="https://img.icons8.com/material-rounded/24/sorting-options.png" alt="sorting-options"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="save"><img width="25" height="25" src="https://img.icons8.com/ios/50/save.png" alt="save"/></div>
-      </li>
-      <li>
-        <div class="tool" @click="load"><img width="25" height="25" src="https://img.icons8.com/fluency-systems-regular/48/load-from-file.png" alt="load-from-file"/></div>
-      </li>
+          <div class="tool" @click="selected = 'line'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/ios/50/line--v1.png"
+              alt="line--v1"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'square'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/ios/50/square-90.png"
+              alt="square-90"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'circle'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/ios/50/circled.png"
+              alt="circled"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'rectangle'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/material-sharp/24/rectangle-stroked.png"
+              alt="rectangle-stroked"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'triangle'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/material-outlined/24/triangle-stroked.png"
+              alt="triangle-stroked"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'ellipse'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/ios/50/ellipse-stroked--v1.png"
+              alt="ellipse-stroked--v1"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'copy'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/material-outlined/24/copy.png"
+              alt="copy"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'paste'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/material-outlined/24/paste.png"
+              alt="paste"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'delete'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/ios/50/trash--v1.png"
+              alt="trash--v1"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'move'">
+            <img width="25" height="25" src="https://img.icons8.com/ios/50/move.png" alt="move" />
+          </div>
+        </li>
+        <li>
+          <div class="tool" id="undo">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/material-outlined/24/undo.png"
+              alt="undo"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" id="redo">
+            <img
+              width="24"
+              height="24"
+              src="https://img.icons8.com/material-outlined/24/redo.png"
+              alt="redo"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="selected = 'options'">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/material-rounded/24/sorting-options.png"
+              alt="sorting-options"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="save">
+            <img width="25" height="25" src="https://img.icons8.com/ios/50/save.png" alt="save" />
+          </div>
+        </li>
+        <li>
+          <div class="tool" @click="load">
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/fluency-systems-regular/48/load-from-file.png"
+              alt="load-from-file"
+            />
+          </div>
+        </li>
       </div>
-      <div><input type="color" name="color" id="color-selector" value="black"></div>
-      <div id="active-box"><span>{{ selected }}</span></div>
-      
+      <div><input type="color" name="color" id="color-selector" value="black" /></div>
+      <div id="active-box">
+        <span>{{ selected }}</span>
+      </div>
     </div>
     <div>
-      <CanvasComp :selected="selected"/>
+      <CanvasComp :selected="selected" @open-dialog="openDialog" ref="applyChangesRef" />
     </div>
-      
   </div>
-
 </template>
 
 <style scoped>
-
-#color-selector{
+#color-selector {
   float: left;
-  margin-top:10px
+  margin-top: 10px;
 }
-#toolbox{
+#toolbox {
   height: 50px;
   border-radius: 24px;
   width: 100%;
   background-color: rgb(8, 127, 141);
-  
 }
-#active-box{
+#active-box {
   float: right;
   margin-right: 100px;
-  padding: 10px 10px 10px 10px ;
+  padding: 10px 10px 10px 10px;
   height: 20px;
-  margin-top:3px;
+  margin-top: 3px;
   border-radius: 5px;
-  border: 2px solid black
+  border: 2px solid black;
 }
-#tool-container{
+#tool-container {
   margin-left: 24px;
 }
-li{
+li {
   float: left;
   list-style-type: none;
-  
 }
-.tool{
+.tool {
   height: 10%;
   padding: 10px;
-  
-  
 }
-.tool:hover{
+.tool:hover {
   background-color: rgb(6, 81, 90);
 }
-.selected{
+.selected {
   background-color: rgb(87, 87, 87);
 }
-
 </style>
--- paint-frontend/src/components/DialogComp.vue
+++ paint-frontend/src/components/DialogComp.vue
@@ -1,20 +1,78 @@
 <template>
-    <dialog  id="wrapper">
-
+  <div class="centertile">
+    <dialog id="wrapper">
+      <div class="title">Change parameters</div>
+      <div class="requirement" v-for="(req, index) in requirements">
+        <div class="property">{{ req }}</div>
+        <input :type="types[index]" :value="defaultValues[index]" v-model="values[index]" />
+      </div>
+      <!-- <button class="cancel">Cancel</button> -->
+      <button class="save" @click="onSave">Save</button>
     </dialog>
+  </div>
 </template>
 <script>
-    export default{
-        name: "DialogComp",
-        mounted(){
-            this.d = document.getElementById("wrapper")
-            this.d.show()
-        }
-
-
+export default {
+  name: 'DialogComp',
+  mounted() {
+    this.d = document.getElementById('wrapper')
+    this.d.show()
+  },
+  data() {
+    return {
+      types: ['color', 'color', 'checkbox', 'number', 'number'],
+      values: this.defaultValues
     }
-
+  },
+  props: {
+    requirements: Array,
+    defaultValues: Array
+  },
+  methods: {
+    onSave() {
+      this.$emit('changeParameters', this.values)
+    }
+  }
+}
 </script>
-<style>
+<style scoped>
+#centerpoint {
+  top: 50%;
+  left: 50%;
+  position: absolute;
+}
 
+dialog {
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -150px;
+  margin-top: -150px;
+  background: rgb(236, 248, 255);
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: bold;
+  margin: auto;
+  width: 100%;
+  text-align: center;
+}
+button {
+  width: 100%;
+  margin: 10 auto;
+  margin-top: 12px;
+  padding: 12px;
+  border-radius: 12px;
+  background-color: rgb(8, 127, 141);
+  color: #ffffff;
+  font-size: 20px;
+}
+
+input {
+  width: 100%;
+}
 </style>
